a952c38f812bdcaf078fd9a680421d5e8fea0907
User <User@LAPTOP-LO3UGFL0>
2018-08-16 22:36:08 +0700

    Initial Commit 2

---
+++ src/jobsheet11/Latihan2.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+import java.util.Scanner;
+public class Latihan2 
+{
+    private static void tampilJudul(String identitas){
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    
+    public static void main(String[] args){
+    
+        String identitas = " Dimas Kenzie Arya Widodo / X RPL 2 / 9 ";
+        
+        tampilJudul(identitas); 
+    }
+    
+    private static String tampilInput(){
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }    
+        String kalimat = tampilInput();
+        
+    private static String vocal2Angka(String kalimat){
+        char [][] arConvert = 
+        {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+        
+        kalimat = kalimat.toLowerCase();
+        for(int i=0;i<arConvert.length;i++)
+            kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+        
+        return kalimat;
+    }
+    String convert = vocal2Angka(kalimat);
+    
+    private static void tampilPerKata(String kalimat, String convert){
+        String[]arrKal = kalimat.split(" ");
+        String[]arrCon = convert.split(" ");
+        
+        for(int i=0;i<arrKal.length;i++)
+            System.out.println(arrKal[i]+" => "+arrCon[i]);
+         tampilPerKata(kalimat,convert);
+         
+    }    
+    
+       
+        
+    private static void tampilHasil(String convert){
+           System.out.println("Kalimat Alay Angka : " + convert);
+           tampilHasil(convert);
+    }
+    
+}
+    
+    
